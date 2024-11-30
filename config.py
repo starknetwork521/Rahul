6db235679d80864c4b0fcb43b85bb575b75875e6
starknetwork521 <starknetwork521@gmail.com>
2024-11-30 18:23:42 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8175244887:AAF6AZdJBNnjltMSaCzkDnFpAlA_IOcQNNI")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("RAHUL945")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5670298051")
+
+
+
+
+
+
+
+  
